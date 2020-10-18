933db03497399db5ed9306d7539a5aeb7b0edcf1
volkangurbuz <volkangurbuz16@gmail.com>
2020-10-18 13:42:24 +0200

    added the project

---
+++ src/main/java/com/volkangurbuz/findmejob/FindMeJobApplication.java
@@ -0,0 +1,13 @@
+package com.volkangurbuz.findmejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMeJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindMeJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/volkangurbuz/findmejob/FindMeJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volkangurbuz.findmejob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMeJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
