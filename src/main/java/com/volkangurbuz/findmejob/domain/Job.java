09515f7d0f2c00a00b82c187643ead86029518de
volkangurbuz <volkangurbuz16@gmail.com>
2020-10-25 14:19:18 +0100

    adjusted objectmapper

--- src/main/java/com/volkangurbuz/findmejob/domain/Job.java
+++ src/main/java/com/volkangurbuz/findmejob/domain/Job.java
@@ -11,12 +11,12 @@ public class Job {
   public String id;
   public String type;
   public String url;
-  public String createdAt;
+  public String created_at;
   public String company;
-  public String companyUrl;
+  public String company_url;
   public String location;
   public String title;
   public String description;
-  public String howToApply;
-  public String companyLogo;
+  public String how_to_apply;
+  public String company_logo;
 }
--- src/main/java/com/volkangurbuz/findmejob/domain/JobDao.java
+++ src/main/java/com/volkangurbuz/findmejob/domain/JobDao.java
@@ -4,14 +4,12 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 public class JobDao {
   private ObjectMapper objectMapper = new ObjectMapper();
 
-  public List<Job> load(String filePath) throws IOException {
-    return Arrays.asList(
-        objectMapper.readValue(this.getClass().getResourceAsStream(filePath), Job[].class));
+  public List<Job> load(String result) throws IOException {
+    return objectMapper.readValue(result, new TypeReference<>() {});
   }
 }
