247c33d0d11b8439adbce1484d332f476a6f0c21
volkangurbuz <volkangurbuz16@gmail.com>
2020-11-11 15:41:07 +0100

    adjusted mongodb

--- resume-sender-service/src/main/java/Test.java
+++ resume-sender-service/src/main/java/Test.java
@@ -1,17 +1,25 @@
 import access.MongoDbConnection;
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import model.Job;
+import com.mongodb.*;
+
+import com.mongodb.client.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.bson.Document;
 
 public class Test {
 
   public static void main(String[] args) {
 
-    String connection =
+    Logger.getLogger("org.mongodb.driver").setLevel(Level.FINE);
+
+    String connectionString =
         "mongodb+srv://volkan:test123@cluster0.dsksp.mongodb.net/findmejob?retryWrites=true&w=majority";
 
-    MongoDatabase database = MongoDbConnection.getInstance(connection).getDatabase("findmejob");
-    ;
+    MongoClient mongoClient = MongoDbConnection.getInstance(connectionString);
   }
 }
--- resume-sender-service/src/main/java/access/MongoDbConnection.java
+++ resume-sender-service/src/main/java/access/MongoDbConnection.java
@@ -1,11 +1,14 @@
 package access;
 
-import com.mongodb.MongoException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.log4j.Logger;
+import org.bson.Document;
+
 import java.net.UnknownHostException;
 
 @Setter
@@ -20,6 +23,7 @@ public class MongoDbConnection {
     if (instance == null) {
       try {
         mongoClient = MongoClients.create(connectionString);
+
       } catch (Exception e) {
         mongoClient = null;
         logger.error("mongoClient" + e.getMessage());
