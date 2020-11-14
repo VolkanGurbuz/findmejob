a815e8e4623c301ebd5101d4a613e2612e34ad87
volkangurbuz <volkangurbuz16@gmail.com>
2020-11-14 22:41:37 +0100

    minor changes

--- resume-sender-service/src/main/java/Test.java
+++ resume-sender-service/src/main/java/Test.java
@@ -1,10 +1,6 @@
-import access.MongoDbConnection;
-import com.mongodb.*;
-
 import com.mongodb.client.*;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.Level;
